f09014fd7e80182e1d149f969f7f432aa3947edb
Storm K <asge0907@easv365.dk>
2021-03-01 12:30:43 +0100

    added session logic to express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,36 @@
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
+import session from 'express-session';
 import 'reflect-metadata';
 import { buildSchema } from 'type-graphql';
-import createDb from './models/context';
+import prisma, { Context } from './models/context';
 import { PostResolver } from './resolvers/postResolver';
 import { UserResolver } from './resolvers/userResolver';
+import { PROD, sessionSecret } from './utils/constants';
 
 const main = async () => {
-	const app = express();
-
 	const apollo = new ApolloServer({
 		schema: await buildSchema({ resolvers: [UserResolver, PostResolver], validate: true }),
-		context: createDb,
+		context: ({ req, res }): Context => ({ prisma, req, res }),
+		debug: !PROD,
 	});
+
+	const app = express();
+
+	app.use(
+		session({
+			cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, // 7 days expiration
+			secret: process.env.PASSWORD ?? sessionSecret,
+			saveUninitialized: false,
+			resave: false,
+			store: new PrismaSessionStore(prisma, {
+				checkPeriod: 2 * 60 * 1000,
+				dbRecordIdIsSessionId: true,
+				dbRecordIdFunction: undefined,
+			}),
+		}),
+	);
 
 	apollo.applyMiddleware({ app });
 
