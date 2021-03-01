df4995d296d898ba1c3952b4c20f3cc64910b865
Storm K <asge0907@easv365.dk>
2021-03-01 12:31:56 +0100

    Fixed interface use from context

--- backend/src/resolvers/postResolver.ts
+++ backend/src/resolvers/postResolver.ts
@@ -1,12 +1,12 @@
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import { Database } from '../models/context';
+import { Context } from './../models/context';
 import { Post } from './../models/post';
 
 @Resolver(Post)
 export class PostResolver {
 	//#region CREATE
 	@Mutation(() => Post)
-	createPost(@Arg('title') title: string, @Arg('content') content: string, @Ctx() { prisma: { post } }: Database) {
+	createPost(@Arg('title') title: string, @Arg('content') content: string, @Ctx() { prisma: { post } }: Context) {
 		return post.create({
 			data: {
 				title,
@@ -19,12 +19,12 @@ export class PostResolver {
 
 	//#region READ
 	@Query(() => [Post], { nullable: true })
-	getPosts(@Ctx() { prisma: { post } }: Database) {
+	getPosts(@Ctx() { prisma: { post } }: Context) {
 		return post.findMany();
 	}
 
 	@Query(() => Post, { nullable: true })
-	getPost(@Arg('id') id: number, @Ctx() { prisma: { post } }: Database) {
+	getPost(@Arg('id') id: number, @Ctx() { prisma: { post } }: Context) {
 		return post.findUnique({ where: { id } });
 	}
 	//#endregion
