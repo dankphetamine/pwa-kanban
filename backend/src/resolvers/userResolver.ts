f130c346b5426813481cb98db38f576b14e4bd91
Storm K <asge0907@easv365.dk>
2021-03-01 12:31:30 +0100

    Updated user resolver, added auth and session logic
    added query to get self if authed

--- backend/src/resolvers/userResolver.ts
+++ backend/src/resolvers/userResolver.ts
@@ -1,28 +1,83 @@
+import argon2id from 'argon2';
 import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import { Database } from '../models/context';
 import { User } from '../models/user';
+import { Text } from '../utils/constants';
+import { Context } from './../models/context';
+import { AuthInput } from './../models/user';
 
 @Resolver(User)
 export class UserResolver {
 	//#region CREATE
+	@Mutation(() => User)
+	async register(
+		@Arg('input') input: AuthInput,
+		@Ctx()
+		{ prisma: { user } }: Context,
+	) {
+		const dbUser = await user.findUnique({ where: { email: input.email } });
 
+		if (dbUser) throw new Error(Text.auth.register.email_taken);
+
+		const dbResponse = await user.create({
+			data: {
+				email: input.email,
+				password: await argon2id.hash(input.password),
+			},
+		});
+
+		if (!dbResponse) throw new Error(Text.auth.register.error);
+
+		return dbResponse;
+	}
+
+	@Mutation(() => User)
+	async login(
+		@Arg('input') input: AuthInput,
+		@Ctx()
+		{ prisma: { user }, req }: Context,
+	) {
+		const dbUser = await user.findUnique({
+			where: { email: input.email },
+		});
+
+		if (!dbUser) throw new Error(Text.auth.login);
+
+		const passMatch = await argon2id.verify(dbUser.password, input.password);
+
+		if (!passMatch) throw new Error(Text.auth.login);
+
+		req.session.userId = dbUser.id.toString();
+
+		return dbUser;
+	}
 	//#endregion
 
 	//#region READ
 	@Query(() => User, { nullable: true })
-	getUser(@Arg('id') id: number, @Ctx() { prisma: { user } }: Database) {
-		return user.findUnique({ where: { id } });
+	getUser(@Arg('email') email: string, @Ctx() { prisma: { user } }: Context) {
+		return user.findUnique({ where: { email } });
 	}
 
 	@Query(() => [User], { nullable: true })
-	getusers(@Ctx() { prisma: { user } }: Database) {
+	getusers(@Ctx() { prisma: { user } }: Context) {
 		return user.findMany({ include: { posts: true } });
+	}
+
+	@Query(() => User, { nullable: true })
+	getSelf(@Ctx() { prisma: { user }, req }: Context) {
+		let id: number;
+
+		typeof req.session.userId === 'string' ? (id = parseInt(req.session.userId)) : (id = req.session.userId);
+
+		console.log('id', id);
+
+		return user.findUnique({ where: { id } });
 	}
 	//#endregion
 
 	//#region UPDATE
 	@Mutation(() => User, { nullable: true })
-	updateDisplayName(@Arg('email') email: string, @Arg('name') name: string, @Ctx() { prisma: { user } }: Database) {
+	updateName(@Arg('email') email: string, @Arg('name') name: string, @Ctx() { prisma: { user } }: Context) {
 		return user.update({
 			where: { email },
 			data: {
@@ -33,6 +88,6 @@ export class UserResolver {
 	//#endregion
 
 	//#region DELETE
-	// ...
-	//#region
+
+	//#endregion
 }
