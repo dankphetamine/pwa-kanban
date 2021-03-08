31b7410576ea2c419c1cb638490045677784c8b7
Storm K <asge0907@easv365.dk>
2021-03-08 13:43:26 +0100

    Updated DB structure & seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,29 @@ const prisma = new PrismaClient();
 const pass = process.env.PASSWORD!;
 
 async function main() {
+	/**
+	 * Order is important, since there is connected a relation which requires previous entries made, to create relation/connect
+	 * Order as follows: Users -> Projects -> Tasks -> Comments (hiearchy)
+	 */
+	await addUsers();
+
+	await addProjects();
+
+	await addTasks();
+
+	await addComments();
+}
+
+main()
+	.catch(e => {
+		console.error(e);
+		process.exit();
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
+
+async function addUsers() {
 	const a = await prisma.user.upsert({
 		where: { email: 'smsj@easv.dk' },
 		update: {
@@ -17,13 +40,6 @@ async function main() {
 			password: await argon2id.hash([pass].reverse().join()),
 			name: 'Søren',
 			image: 'https://www.easv.dk/app/uploads/2017/09/SMSJ_06_150x150_acf_cropped_quality-85.jpg',
-			posts: {
-				create: {
-					title: 'Lorem ipsum dolor sit amet.',
-					content: 'Ut sem metus, auctor tincidunt elementum',
-					published: true,
-				},
-			},
 		},
 	});
 	const b = await prisma.user.upsert({
@@ -38,21 +54,6 @@ async function main() {
 			password: await argon2id.hash([pass].reverse().join()),
 			name: 'Kristian',
 			image: 'https://www.easv.dk/app/uploads/2017/09/KW_03_150x150_acf_cropped_quality-85.jpg',
-			posts: {
-				create: [
-					{
-						title: 'Nunc ullamcorper mauris non magna egestas',
-						content: 'Sed eget sodales ex, id porta nisi. Aliquam vitae magna id nisi faucibus dignissim ac ac sapien',
-						published: true,
-					},
-					{
-						title: 'Vestibulum blandit ligula et eros tincidunt',
-						content:
-							'Sed eget felis et tortor scelerisque convallis non non turpis. In id sollicitudin lectus, quis euismod enim. Curabitur sagittis, felis eu porta facilisis, turpis quam aliquet enim, vel tincidunt mauris neque ac nisl. Aliquam eu porta mauris. Ut a ultrices diam',
-						published: true,
-					},
-				],
-			},
 		},
 	});
 
@@ -70,39 +71,156 @@ async function main() {
 			name: 'Asger Storm',
 			image:
 				'https://cdn.shopify.com/s/files/1/0160/2840/1712/products/cheems_characterai_ry-min_739x.png?v=1606473176',
-			posts: {
-				create: [
-					{
-						title: 'Amazing',
-						content: "I'm on a seafood diet. I see food and I eat it.",
-						published: true,
-					},
-					{
-						title: 'Great',
-						content: 'Why did the scarecrow win an award? Because he was outstanding in his field.',
-						published: true,
-					},
-					{
-						title: 'Cool',
-						content: 'I made a pencil with two erasers. It was pointless.',
-						published: true,
-					},
-					{
-						title: 'Soon',
-						content: "I've got a great joke about construction, but I'm still working on it.",
-						published: true,
-					},
-				],
-			},
 		},
 	});
-	console.log({ a, b, c });
+
+	console.log('Added users: ', { a, b, c });
 }
-main()
-	.catch(e => {
-		console.error(e);
-		process.exit();
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
+
+async function addProjects() {
+	const a = await prisma.project.upsert({
+		where: { id: 1 },
+		update: {
+			name: 'Project 1',
+			description: 'Description 1',
+			owner: { connect: { id: 1 } },
+		},
+		create: {
+			name: 'Project 1',
+			description: 'Description 1',
+			owner: { connect: { id: 1 } },
+		},
 	});
+
+	const b = await prisma.project.upsert({
+		where: { id: 2 },
+		update: {
+			name: 'Project 2',
+			description: 'Description 2',
+			owner: { connect: { id: 2 } },
+		},
+		create: {
+			name: 'Project 2',
+			description: 'Description 2',
+			owner: { connect: { id: 2 } },
+		},
+	});
+
+	const c = await prisma.project.upsert({
+		where: { id: 3 },
+		update: {
+			name: 'Project 3',
+			owner: { connect: { id: 3 } },
+		},
+		create: {
+			name: 'Project 3',
+			owner: { connect: { id: 3 } },
+		},
+	});
+
+	console.log('Added projects: ', { a, b, c });
+}
+
+async function addTasks() {
+	const a = await prisma.task.upsert({
+		where: { id: 1 },
+		update: {
+			title: 'Task 1',
+			description: 'Description 1',
+			project: { connect: { id: 1 } },
+			reporter: { connect: { id: 1 } },
+			asignee: { connect: { id: 2 } },
+		},
+		create: {
+			title: 'Task 1',
+			description: 'Description 1',
+			project: { connect: { id: 1 } },
+			reporter: { connect: { id: 1 } },
+			asignee: { connect: { id: 2 } },
+		},
+	});
+
+	const b = await prisma.task.upsert({
+		where: { id: 2 },
+		update: {
+			title: 'Task 2',
+			description: 'Description 2',
+			project: { connect: { id: 2 } },
+			reporter: { connect: { id: 2 } },
+			asignee: { connect: { id: 1 } },
+		},
+		create: {
+			title: 'Task 2',
+			description: 'Description 2',
+			project: { connect: { id: 2 } },
+			reporter: { connect: { id: 2 } },
+			asignee: { connect: { id: 1 } },
+		},
+	});
+
+	const c = await prisma.task.upsert({
+		where: { id: 3 },
+		update: {
+			title: 'Task 3',
+			description: 'Description 3',
+			project: { connect: { id: 3 } },
+			reporter: { connect: { id: 3 } },
+			asignee: { connect: { id: 3 } },
+		},
+		create: {
+			title: 'Task 3',
+			description: 'Description 3',
+			project: { connect: { id: 3 } },
+			reporter: { connect: { id: 3 } },
+			asignee: { connect: { id: 3 } },
+		},
+	});
+
+	console.log('Added tasks: ', { a, b, c });
+}
+
+async function addComments() {
+	const a = await prisma.comment.upsert({
+		where: { id: 1 },
+		update: {
+			text: 'Top text, Bottom text',
+			author: { connect: { id: 1 } },
+			task: { connect: { id: 1 } },
+		},
+		create: {
+			text: 'Top text, Bottom text',
+			author: { connect: { id: 1 } },
+			task: { connect: { id: 1 } },
+		},
+	});
+
+	const b = await prisma.comment.upsert({
+		where: { id: 2 },
+		update: {
+			text: 'Comment 2',
+			author: { connect: { id: 2 } },
+			task: { connect: { id: 2 } },
+		},
+		create: {
+			text: 'Comment 2',
+			author: { connect: { id: 2 } },
+			task: { connect: { id: 2 } },
+		},
+	});
+
+	const c = await prisma.comment.upsert({
+		where: { id: 3 },
+		update: {
+			text: 'Comment 3',
+			author: { connect: { id: 3 } },
+			task: { connect: { id: 3 } },
+		},
+		create: {
+			text: 'Comment 3',
+			author: { connect: { id: 3 } },
+			task: { connect: { id: 3 } },
+		},
+	});
+
+	console.log('Added comments: ', { a, b, c });
+}
