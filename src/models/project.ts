aea79ca68522ca055329fd32ce7fceeaf07ae520
Storm K <asge0907@easv365.dk>
2021-03-09 17:18:43 +0100

    Updated project model

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { Field, ID, ObjectType } from 'type-graphql';
+import { Task } from './task';
 import { User } from './user';
 
 @ObjectType()
@@ -7,20 +8,20 @@ export class Project {
 	@Field(_type => ID)
 	id: number;
 
-	@Field(() => User)
-	owner: User;
-
-	@Field(() => User)
-	reporter: User;
-
-	@Field(() => User, { nullable: true })
-	asignee?: User;
-
 	@Field()
 	name: string;
 
 	@Field({ nullable: true })
 	description?: string;
+
+	@Field(() => User)
+	owner: User;
+
+	@Field(() => [User], { nullable: true })
+	collaborators?: [User];
+
+	@Field(() => [Task], { nullable: true })
+	tasks?: [Task];
 
 	@Field()
 	createdAt: Date;
