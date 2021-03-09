17b5419728de27a52c15a81e583af273b5ce3694
Storm K <asge0907@easv365.dk>
2021-03-09 20:59:06 +0100

    outcommented postresolver

--- src/resolvers/postResolver.ts
+++ src/resolvers/postResolver.ts
@@ -1,50 +1,50 @@
-import { AuthenticationError } from 'apollo-server-express';
-import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
-import { Context } from '../models/context';
-import { Post, PostFilterInput } from '../models/post';
-import { Text } from '../utils/constants';
+// import { AuthenticationError } from 'apollo-server-express';
+// import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+// import { Context } from '../models/context';
+// import { Post, PostFilterInput } from '../models/post';
+// import { Text } from '../utils/constants';
 
-@Resolver(Post)
-export class PostResolver {
-	//#region CREATE
-	@Mutation(() => Post)
-	createPost(@Ctx() { prisma: { post }, req }: Context, @Arg('title') title: string, @Arg('content') content: string) {
-		if (!req.session.userId) throw new AuthenticationError(Text.auth.notLoggedIn);
+// @Resolver(Post)
+// export class PostResolver {
+// 	//#region CREATE
+// 	@Mutation(() => Post)
+// 	createPost(@Ctx() { prisma: { post }, req }: Context, @Arg('title') title: string, @Arg('content') content: string) {
+// 		if (!req.session.userId) throw new AuthenticationError(Text.auth.notLoggedIn);
 
-		return post.create({
-			data: {
-				title,
-				content,
-				userId: 3,
-			},
-			include: { author: true },
-		});
-	}
-	//#endregion
+// 		return post.create({
+// 			data: {
+// 				title,
+// 				content,
+// 				userId: 3,
+// 			},
+// 			include: { author: true },
+// 		});
+// 	}
+// 	//#endregion
 
-	//#region READ
-	@Query(() => [Post], { nullable: true })
-	posts(@Ctx() { prisma: { post } }: Context, @Arg('filter', { nullable: true }) filter?: PostFilterInput) {
-		return post.findMany({
-			take: filter?.limit,
-			skip: filter?.offset,
-			where: { userId: filter?.userId },
-			include: { author: true },
-			orderBy: { updatedAt: 'desc' },
-		});
-	}
+// 	//#region READ
+// 	@Query(() => [Post], { nullable: true })
+// 	posts(@Ctx() { prisma: { post } }: Context, @Arg('filter', { nullable: true }) filter?: PostFilterInput) {
+// 		return post.findMany({
+// 			take: filter?.limit,
+// 			skip: filter?.offset,
+// 			where: { userId: filter?.userId },
+// 			include: { author: true },
+// 			orderBy: { updatedAt: 'desc' },
+// 		});
+// 	}
 
-	@Query(() => Post, { nullable: true })
-	post(@Ctx() { prisma: { post } }: Context, @Arg('id') id: number) {
-		return post.findUnique({ where: { id } });
-	}
-	//#endregion
+// 	@Query(() => Post, { nullable: true })
+// 	post(@Ctx() { prisma: { post } }: Context, @Arg('id') id: number) {
+// 		return post.findUnique({ where: { id } });
+// 	}
+// 	//#endregion
 
-	//#region UPDATE
-	//#endregion
+// 	//#region UPDATE
+// 	//#endregion
 
-	//#region DELETE
-	// @Mutation(() => Post, { nullable: true })
-	// deletePost;
-	//#endregion
-}
+// 	//#region DELETE
+// 	// @Mutation(() => Post, { nullable: true })
+// 	// deletePost;
+// 	//#endregion
+// }
